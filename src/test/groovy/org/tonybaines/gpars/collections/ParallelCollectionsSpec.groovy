8dbbe5e02ee010166b4fb17995f15ca35395db19
Tony Baines <tony.baines.1@gmail.com>
2011-03-13 20:55:18 +0000

    Simplified the spec to make it clearer

--- src/test/groovy/org/tonybaines/gpars/collections/ParallelCollectionsSpec.groovy
+++ src/test/groovy/org/tonybaines/gpars/collections/ParallelCollectionsSpec.groovy
@@ -10,33 +10,24 @@ class ParallelCollectionsSpec extends Specification {
 
 	@Timeout(5) // should fail
 	def "processing results sequentially from the Bing news feed"() {
-		given:
+		expect:
 		def feed = BingNews.feed
-		def characterCount = 0
-		when:
 		def linkTexts = feed.channel.item.collect { item ->
 			item.link.text().toURL().text
 		}
-		characterCount = linkTexts.sum { it.size() }
-		then:
-		characterCount > 100
 	}
 
 	@Timeout(5)
 	def "processing results in parallel from the Bing news feed"() {
-		given:
+		expect:
 		def feed = BingNews.feed
-		def characterCount = 0
-		when:
 		// Metaclass magic to add the parallel collection methods
+		// Uses ParallelArray-based (from JSR-166y) implementation
+		// (not Java Threads)
 		withPool {
 			def linkTexts = feed.channel.item.collectParallel { item ->
 				item.link.text().toURL().text
 			}
-			characterCount = linkTexts.sum { it.size() }
 		}
-		
-		then:
-		characterCount > 100
 	}
 }
